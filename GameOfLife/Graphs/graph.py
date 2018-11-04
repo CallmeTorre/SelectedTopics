636090357b5d8cc8e120d3eb0599b5ce73bc2552
CallmeTorre <alexis_torreblanca@outlook.com>
2018-11-04 15:10:34 -0600

    Added graphs attractors

---
+++ GameOfLife/Graphs/graph.py
@@ -0,0 +1,17 @@
+import json
+
+from graphviz import Digraph
+
+with open('4x4.json', 'r') as f:
+    nodes_dict = json.load(f)
+
+f = Digraph('finite_state_machine', filename='fsm.gv', engine='sfdp')
+f.attr('node', shape='circle')
+
+for edge in nodes_dict['edges']:
+    data = edge['data']
+    source = data['source']
+    target = data['target']
+    f.edge(source, target)
+
+f.view()
